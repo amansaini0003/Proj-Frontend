6c9f55979b0f79edbfefd12693e78c8a8c586a90
Aman Saini <amansaini0003@gmail.com>
2022-05-17 05:00:29 +0530

    added restrictions on admin routes

--- src/core/Navigation.js
+++ src/core/Navigation.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth/helper";
 
 const currentTab = (history, path) => {
   if (history.location.pathname === path) {
@@ -44,33 +45,45 @@ const Navigation = ({ history }) => (
           A. DashBoard
         </Link>
       </li>
-      <li className="nav-items">
-        <Link
-          style={currentTab(history, "/signup")}
-          className="nav-link"
-          to="/signup"
-        >
-          SignUp
-        </Link>
-      </li>
-      <li className="nav-items">
-        <Link
-          style={currentTab(history, "/signin")}
-          className="nav-link"
-          to="/signin"
-        >
-          SignIn
-        </Link>
-      </li>
-      <li className="nav-items">
-        <Link
-          style={currentTab(history, "/signout")}
-          className="nav-link"
-          to="/signout"
-        >
-          SignOut
-        </Link>
-      </li>
+
+      {!isAuthenticated() && (
+        <Fragment>
+          <li className="nav-items">
+            <Link
+              style={currentTab(history, "/signup")}
+              className="nav-link"
+              to="/signup"
+            >
+              SignUp
+            </Link>
+          </li>
+
+          <li className="nav-items">
+            <Link
+              style={currentTab(history, "/signin")}
+              className="nav-link"
+              to="/signin"
+            >
+              SignIn
+            </Link>
+          </li>
+        </Fragment>
+      )}
+
+      {isAuthenticated() && (
+        <li className="nav-items">
+          <span
+            className="nav-link text-warning"
+            onClick={() => {
+              signout(() => {
+                history.push("/");
+              });
+            }}
+          >
+            SignOut
+          </span>
+        </li>
+      )}
     </ul>
   </div>
 );
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -6,8 +6,8 @@ import { signin, authenticate, isAuthenticated } from "../auth/helper";
 
 const Signin = () => {
   const [values, setValues] = useState({
-    email: "",
-    password: "",
+    email: "rachit@cogoport.com",
+    password: "12345678",
     error: "",
     loading: false,
     didRedirect: false,
