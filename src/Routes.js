015dc090dbdaefff24b27fec4db4ba3450a72d6c
Aman Saini <amansaini0003@gmail.com>
2022-05-17 16:39:05 +0530

    added create categories

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,7 @@ import Signin from "./user/Signin";
 import Signup from "./user/Signup";
 import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
 
 export default function Routes() {
   return (
@@ -17,6 +18,11 @@ export default function Routes() {
         <Route path="/signin" exact component={Signin} />
         <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+        <AdminRoute
+          path="/admin/create/category"
+          exact
+          component={AddCategory}
+        />
       </Switch>
     </Router>
   );
---
+++ src/admin/AddCategory.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { isAuthenticated } from "../auth/helper";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { createCategory } from "./helper/adminapicall";
+
+const AddCategory = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const { user, token } = isAuthenticated();
+
+  const goBack = () => {
+    return (
+      <div className="mt-5">
+        <Link className="btn btn-sm btn-info mb-3" to="/admin/dashboard">
+          Admin Home
+        </Link>
+      </div>
+    );
+  };
+
+  const handleChange = (event) => {
+    setError("");
+    setName(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setError("");
+    setSuccess(false);
+
+    // backend req fired
+
+    createCategory(user._id, token, { name })
+      .then((data) => {
+        if (data.error) {
+          setError(true);
+        } else {
+          setError(false);
+          setSuccess(true);
+          setName("");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const sucessMessage = () => {
+    if (success) {
+      return <h4 className="text-success">Category created successfully</h4>;
+    }
+  };
+
+  const errorMessage = () => {
+    if (error) {
+      return <h4 className="text-success">Failed to create category</h4>;
+    }
+  };
+
+  const myCategoryForm = () => {
+    return (
+      <form>
+        <div className="form-group">
+          <p className="lead">Enter the Category</p>
+          <input
+            type="text"
+            className="form-control my-3"
+            onChange={handleChange}
+            value={name}
+            autoFocus
+            required
+            placeholder="For Ex. Summer"
+          />
+          <button onClick={onSubmit} className="btn btn-outline-info">
+            Create Category
+          </button>
+        </div>
+      </form>
+    );
+  };
+
+  return (
+    <div>
+      <Base
+        title="Create a category here"
+        description="Add a new Category for new T-Shirt"
+        className="container bg-info p-4"
+      >
+        <div className="row bg-white rounded">
+          <div className="col-md-8 offset-md-2">
+            {sucessMessage()}
+            {errorMessage()}
+            {myCategoryForm()} {goBack()}
+          </div>
+        </div>
+      </Base>
+    </div>
+  );
+};
+
+export default AddCategory;
