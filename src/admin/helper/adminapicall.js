c0f5600ae296b90cc8fbcd0b3954954a4a5fc4f9
Aman Saini <amansaini0003@gmail.com>
2022-05-17 17:09:51 +0530

    added CRUD for products

--- src/admin/helper/adminapicall.js
+++ src/admin/helper/adminapicall.js
@@ -1,17 +1,107 @@
 import { API } from "../../backend";
 
-export const createCategory = (userId, token, category) => {
-  return fetch(`${API}/category/create/${userId}`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(category),
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .catch((err) => console.log(err, "error in create category"));
+//category calls
+export const createCategory = async (userId, token, category) => {
+  try {
+    const res = await fetch(`${API}/category/create/${userId}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(category),
+    });
+    return await res.json();
+  } catch (err) {
+    return console.log(err, "error in create category");
+  }
+};
+
+// get all categories
+export const getCategories = async () => {
+  try {
+    const res = await fetch(`{API}/categories`, {
+      method: "GET",
+    });
+    return await res.json();
+  } catch (err) {
+    return console.log(err, "error in get category");
+  }
+};
+
+//product calls
+
+//create a product
+export const createProduct = async (userId, token, product) => {
+  try {
+    const res = await fetch(`${API}/product/create/${userId}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: product,
+    });
+    return await res.json();
+  } catch (err) {
+    return console.log(err, "error in create product");
+  }
+};
+
+//get all products
+export const getProducts = async () => {
+  try {
+    const res = await fetch(`{API}/products`, {
+      method: "GET",
+    });
+    return await res.json();
+  } catch (err) {
+    return console.log(err, "error in get products");
+  }
+};
+
+//delete a product
+export const deleteProduct = async (productId, userId, token, product) => {
+  try {
+    const res = await fetch(`${API}/product/${productId}/${userId}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return await res.json();
+  } catch (err) {
+    return console.log(err, "error in delete product");
+  }
+};
+
+// get a product
+export const getProduct = async (productId) => {
+  try {
+    const res = await fetch(`${API}/product/${productId}`, {
+      method: "GET",
+    });
+    return await res.json();
+  } catch (err) {
+    return console.log(err, "error in get a product");
+  }
+};
+
+//update a product
+export const updateProduct = async (productId, userId, token, product) => {
+  try {
+    const res = await fetch(`${API}/product/${productId}/${userId}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: product,
+    });
+    return await res.json();
+  } catch (err) {
+    return console.log(err, "error in update product");
+  }
 };
